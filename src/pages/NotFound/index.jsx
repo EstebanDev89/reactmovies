6bced6d7b4509f97b6fe93558507899bb00c7190
Esteban Sileoni <esteban.sileoni@gmail.com>
2022-02-15 20:30:36 -0300

    feature: back to the future

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './styles.css'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <div className="not-found">
+        <section className="not-found__section">
+          <div className="not-found__images">
+            <img loading="lazy" className="not-found__images__image" src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage" />
+            <img loading="lazy" className="not-found__images__image" src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly" />
+          </div>
+          <div className="not-found__titles">
+            <h1 className="not-found__titles__title--1">404</h1>
+            <h2 className="not-found__titles__title--2">PAGE NOT FOUND</h2>
+            <h3 className="not-found__titles__title--3">BACK TO HOME?</h3>
+            <Link to="/" className="not-found__link--yes"> YES </Link>
+            <a href='https://www.youtube.com/watch?v=G3AfIvJBcGo'
+              className="not-found__link"> NO </a>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
