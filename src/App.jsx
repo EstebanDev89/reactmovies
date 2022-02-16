95560034570126ffecea07b4f5d48b9242939b6e
Esteban Sileoni <esteban.sileoni@gmail.com>
2022-02-16 13:58:57 -0300

    feature: list movies and redirect to a movie

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import MovieDetails from './pages/MovieDetails';
 import NotFound from './pages/NotFound';
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { loadingContext } from './contexts/LoadingContext'
 import './App.css';
@@ -12,18 +13,29 @@ function App() {
   const { isLoading } = useContext(loadingContext)
 
   return (
-    <>      
+    <>
+      <BrowserRouter>
         {isLoading ? <Loading /> : null}
-        <BrowserRouter>
+        <header>
+          <Link to="/"> Home </Link>
+          <Link to="/other"> NotFound </Link>
+        </header>
+        <main>
+
           <Routes>
             {/* <Route path="/playground" element={<Loading />} /> */}
             <Route path="/" element={<Home />} />
-            <Route path="/movie-details/:id" element={<MovieDetails />} />
+            <Route path="/details/:id" element={<MovieDetails />} />
             <Route path="/not-found" element={<NotFound />} />
             <Route path="*" element={<Navigate to="/not-found" replace />} />
             {/* use replace for non url redirection and support back page on browser */}
           </Routes>
-        </BrowserRouter>
+
+        </main>
+        <footer>
+          REDES SOCIALES
+        </footer>
+      </BrowserRouter>
 
     </>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,17 @@
 import { MoviesActions } from '../../actions/moviesActions';
 import { loadingContext } from '../../contexts/LoadingContext'
 import React, { useEffect, useReducer, useContext } from 'react'
-import { Link } from 'react-router-dom'
 import moviesReducer, { moviesInitialState } from '../../reducers/moviesReducer';
+import './styles.css'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     const token = process.env.REACT_APP_MOVIE_PUBLIC_TOKEN;
     const server = process.env.REACT_APP_MOVIE_BASE_API_URL;
     const nowPlayingURL = `${server}/3/movie/popular?language=es-MX&page=1&region=AR`;
+    const imageWitdh = 'w300';
+    const baseImageURL = `${process.env.REACT_APP_MOVIE_IMAGE_BASE_URL}${imageWitdh}`;
+
     const [moviesState, dispatchMovies] = useReducer(moviesReducer, moviesInitialState)
     const { setLoading } = useContext(loadingContext);
 
@@ -23,12 +27,34 @@ export default function Home() {
             })
     }, [])
 
+    let movie = {}
+
     return (
         <>
             {console.log(moviesState)}
-            <Link to="/"> Home </Link>
-            <Link to="/movie-details/1"> Details </Link>
-            <Link to="/other"> NotFound </Link>
+            {
+                moviesState.movies.map(
+                    movie =>
+                        <section>
+                            <article>
+                                <div>{movie?.title}</div>
+                                <div>
+                                    <Link to={`/details/${movie.id}`}>
+                                        <div>
+                                            <img src={`${baseImageURL}${movie?.poster_path}`} alt={movie?.title} loading="lazy"></img>
+                                        </div>
+                                    </Link>
+
+                                </div>
+                                <div>
+                                    <div className="star"></div>
+                                    <span>{movie?.vote_average ? movie?.vote_average : 0}</span>
+                                </div>
+                            </article>
+                        </section>
+                )
+            }
+
         </>
     )
 }
